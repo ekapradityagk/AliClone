9e53108dd6191b91815c6f47f8d9e7cbf1ef8495
ekapradityagk <ekapradityagk@gmail.com>
2023-07-30 23:30:42 +0700

    Initial Commit

---
+++ AliClone/AliCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AliCloneApp.swift
+//  AliClone
+//
+//  Created by Aisa Utomo on 30/07/23.
+//
+
+import SwiftUI
+
+@main
+struct AliCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
